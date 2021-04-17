6999403bf045302d01cfff19da00d2cd5dbbd81c
Darry Morales <darry.morales@pragma.com.co>
2021-04-17 10:57:22 -0500

    added DTO, Mapper and Controller layer to Customer Entity

---
+++ src/main/java/co/edu/eafit/bank/controller/CustomerController.java
@@ -0,0 +1,91 @@
+package co.edu.eafit.bank.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.eafit.bank.domain.Customer;
+import co.edu.eafit.bank.dto.CustomerDTO;
+import co.edu.eafit.bank.mapper.CustomerMapper;
+import co.edu.eafit.bank.service.CustomerService;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+
+	@Autowired
+	CustomerService customerService;
+	
+	@Autowired
+	CustomerMapper customerMapper;
+	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<CustomerDTO> detele(@PathVariable("id") Integer id) throws Exception {
+		
+		customerService.deleteById(id);
+		
+		return ResponseEntity.ok().build();
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<CustomerDTO> save(@Valid @RequestBody CustomerDTO customerDTO) throws Exception {
+		
+		Customer customer = customerMapper.customerDTOtoCustomer(customerDTO);
+		customer = customerService.save(customer);
+		
+		customerDTO = customerMapper.customerToCustomerDTO(customer);
+		
+		return ResponseEntity.ok().body(customerDTO);
+	}
+	
+	@PutMapping
+	public ResponseEntity<CustomerDTO> update(@Valid @RequestBody CustomerDTO customerDTO) throws Exception {
+		
+		Customer customer = customerMapper.customerDTOtoCustomer(customerDTO);
+		customer = customerService.update(customer);
+		
+		customerDTO = customerMapper.customerToCustomerDTO(customer);
+		
+		return ResponseEntity.ok().body(customerDTO);
+	}
+	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CustomerDTO> findById(@PathVariable("id") Integer id) {
+		
+		Optional<Customer> customerOptional = customerService.findById(id);
+		
+		Customer customer = (customerOptional.isPresent()==true) ? customerOptional.get() : null;
+		
+		CustomerDTO customerDTO = customerMapper.customerToCustomerDTO(customer);
+		
+		
+		return ResponseEntity.ok().body(customerDTO);
+	}
+
+	@GetMapping()
+	public ResponseEntity<List<CustomerDTO>> findAll(){
+		
+		List<Customer> customers=customerService.findAll();
+		List<CustomerDTO> customerDTOs = customerMapper.customerToCustomersDTOs(customers);
+		
+		return ResponseEntity.ok().body(customerDTOs);
+	}
+
+	
+}
